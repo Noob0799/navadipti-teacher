9cbc5343387653e232f1463d058b4803be6d8792
Noob0799 <uchihaitachi0799@gmail.com>
2020-04-15 01:07:22 +0530

    Prep for deploy 14

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect("mongodb+srv://noob:XHfaVoZsq8AvbqC8@cluster0-atyzx.mongodb.net
     console.log("Connection failed!");
   });
 
-app.use((req,res,next) => {
+app.use('*', (req,res,next) => {
   res.setHeader("Access-Control-Allow-Origin","*");
   res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Accept, Content-type");
   res.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, PATCH, DELETE, OPTIONS");
